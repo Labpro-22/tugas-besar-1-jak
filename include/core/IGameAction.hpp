bdd129f74ffb67c746faf4696c04c6b79cca0b66
SpicyCoinCracker <13524138@mahasiswa.itb.ac.id>
2026-04-21 10:45:57 +0700

    feat/ added IGameActcion

---
+++ include/core/IGameAction.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+enum class GamePhase
+{
+    NORMAL,
+    ACTION,
+    JAIL_CHOICE
+};
+
+class IGameAction
+{
+public:
+    virtual ~IGameAction() = default;
+
+    // Phase
+    virtual bool isGameActive() const = 0;
+    virtual GamePhase getCurrentState() const = 0;
+
+    // Dadu
+    virtual void rollDice() = 0;
+    virtual void setDice(int x, int y) = 0;
+
+    // Property
+    virtual void buyCurrentProperty() = 0;
+    virtual void mortgageProperty(const std::string &code) = 0;
+    virtual void redeemProperty(const std::string &tileCode) = 0;
+    virtual void buildOnProperty(const std::string &tileCode) = 0;
+    virtual void sellBuildingOnProperty(const std::string &tileCode) = 0;
+
+    // Auction
+    virtual void placeBid(int amount) = 0;
+    virtual void passAuction() = 0;
+
+    // SkillCard
+    virtual void useSkillCard(const std::string &cardIndex) = 0;
+
+    // Jail
+    virtual void payJailFine() = 0;
+    virtual void useJailFreeCard() = 0;
+
+    // SaveLoad
+    virtual void saveGame(const std::string &filename) = 0;
+    virtual void loadGame(const std::string &filename) = 0;
+
+    // Display
+    virtual void printBoard() = 0;
+    virtual void printDeed(const std::string &tileCode) = 0;
+    virtual void printPropertyInventory() = 0;
+    virtual void printLog(int limit) = 0;
+};
